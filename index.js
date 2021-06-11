c9d84c916499449ea7192eec6f07218766758665
jopemachine <jopemachine@naver.com>
2021-06-11 22:35:57 +0900

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const arvish = require('@jopemachine/arvish');
+
+(async () => {
+  const data = await arvish.fetch('https://repo.packagist.org/search.json', {
+    query: {
+      q: arvish.input
+    }
+  });
+
+  const items = data.results.map(pkg => {
+    return {
+      title: pkg.name,
+      subtitle: pkg.description,
+      arg: pkg.repository || pkg.url,
+      mods: {
+        alt: {
+          arg: pkg.url,
+          subtitle: 'Open the Packagist page instead of the GitHub repo'
+        },
+        ctrl: {
+          arg: pkg.name,
+          subtitle: 'Copy package name'
+        }
+      },
+      quicklookurl: pkg.repository && `${pkg.repository}#readme`
+    };
+  });
+
+  arvish.output(items);
+})();
